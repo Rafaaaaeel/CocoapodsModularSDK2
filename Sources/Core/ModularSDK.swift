349b596b796c36e656c7ab73822683bbd058dc8d
Rafael Oliveira pires <74843118+Rafaaaaeel@users.noreply.github.com>
2025-12-09 18:59:30 -0300

    fix

--- Sources/Core/ModularSDK.swift
+++ Sources/Core/ModularSDK.swift
@@ -1,14 +1,18 @@
 public enum ModularSDK {
     public static func start() {
-        run("ModularSDK_Chat.Chat")
-        run("ModularSDK_Network.Network")
+        run("ModularSDK.Chat")
+        run("ModularSDK.Network")
     }
 }
 
 private extension ModularSDK {
 
     static func run(_ className: String) {
-        guard let type = NSClassFromString(className) as? NSObject.Type else { return }
+        guard let type = NSClassFromString(className) as? NSObject.Type else {
+            print("Class not found:", className)
+            return
+        }
+        
         let instance = type.init()
         instance.perform(NSSelectorFromString("execute"))
     }
