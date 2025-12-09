e797f14831b4564621a1abf943979cda24d5a201
Rafael Oliveira pires <74843118+Rafaaaaeel@users.noreply.github.com>
2025-12-09 18:50:08 -0300

    fix

--- Sources/Core/ModularSDK.swift
+++ Sources/Core/ModularSDK.swift
@@ -1,9 +1,21 @@
-import Foundation
-
-public class ModularSDK {
-    public init() {}
-
+public enum ModularSDK {
     public static func start() {
-        print("ModularSDK started")
+        runChatIfAvailable()
+        runNetworkIfAvailable()
+    }
+}
+
+private extension ModularSDK {
+
+    static func runChatIfAvailable() {
+        guard let chatType = NSClassFromString("ChatModule.Chat") as? NSObject.Type else { return }
+        let instance = chatType.init()
+        instance.perform(NSSelectorFromString("execute"))
+    }
+
+    static func runNetworkIfAvailable() {
+        guard let netType = NSClassFromString("NetworkModule.Network") as? NSObject.Type else { return }
+        let instance = netType.init()
+        instance.perform(NSSelectorFromString("execute"))
     }
 }
